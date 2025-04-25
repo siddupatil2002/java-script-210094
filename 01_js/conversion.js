4f85ff1141dbfe1d6f235ff523a9a1e55068c5eb
siddupatil2002 <97458297+siddupatil2002@users.noreply.github.com>
2025-04-25 18:02:54 +0000

    conversion

---
+++ 01_js/conversion.js
@@ -0,0 +1,31 @@
+let score="true"
+
+//console.log(score)
+//console.log(typeof score)
+
+let valueInNumber=Number(score)
+//console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+ /*
+ when we are converting into Number
+ "33"=>33
+ "33abc"=>Nan but type is number
+ "true"=>1."false"=>0
+*/
+
+let userLoggedIn=0
+//console.log(userLoggedIn)
+
+let booleanLoggedIn=Boolean(userLoggedIn)
+//console.log(booleanLoggedIn)
+//console.log(typeof booleanLoggedIn)
+// the above values are converted into boolean from number,string,empty string
+//0=>true,0=>false
+// ""=>false
+//"siddu"=>true
+
+let someNumber=55
+console.log(someNumber)
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
