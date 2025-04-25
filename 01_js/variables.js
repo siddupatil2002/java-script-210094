a90564d029f4242e73ae4a2198d7b72a7b13813e
siddupatil2002 <97458297+siddupatil2002@users.noreply.github.com>
2025-04-25 17:39:38 +0000

    variables

---
+++ 01_js/variables.js
@@ -0,0 +1,23 @@
+const accountId=122334
+let accountName="siddu"
+let accountEmail="siddu@gmail.com"
+var accountpassword="123456"
+accountCity="Bidar"
+let accountState;
+//accountId=3 not allowed beacuse it is fixed; 
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountName,accountEmail,accountId,accountpassword,accountCity,accountState])
+
+/*
+prefer not use var
+because of issue in block scope and functional scope*/
+ 
+
+
+
+
+
+
+
+
