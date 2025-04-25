fddc222865cecd073d482fda3c50d42c55587d35
siddupatil2002 <97458297+siddupatil2002@users.noreply.github.com>
2025-04-25 17:39:18 +0000

    datatypes

---
+++ 01_js/datatypes.js
@@ -0,0 +1,23 @@
+//"use strict " //it will update to new js versions
+// alert(3+3) //error when executed in node in js but it will execute in window browser;
+
+//datatypes
+
+/*
+number
+bigint
+string
+boolean
+null-stand alone value
+symbol-used for unique
+undefined
+
+object
+*/
+let fname;
+age=null
+console.log(typeof null)
+console.log(typeof undefined)
+
+//type of undefined is undefined
+// type of null is object
